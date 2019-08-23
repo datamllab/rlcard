375ce86b780abaaf71b5ef66cf8c46c59e1c3b4b
Ruzhe Wei <ruzhe.wei@foxmail.com>
2019-08-23 03:00:02 +0000

    Update rlcard/games/texas-holdem/dealer.py, rlcard/games/texas-holdem/game.py, rlcard/games/texas-holdem/judger.py, rlcard/games/texas-holdem/player.py, rlcard/games/texas-holdem/round.py, rlcard/envs/texasholdem.py files

--- rlcard/games/texas-holdem/dealer.py
+++ rlcard/games/texas-holdem/dealer.py
@@ -1,22 +1,66 @@
 # -*- coding: utf-8 -*-
 """Implement Texasholdem Dealer class"""
 import random
-from core import Dealer
-from utils.utils import init_standard_deck
+from rlcard.core import Dealer
+from rlcard.utils.utils import init_standard_deck
 from itertools import product
 import random
+import copy
 
-DECK = init_standard_deck()
 
 class TexasDealer(Dealer):
-    def __init__(self):
-        self._cards = []
-        self._random_card =[]
-    def reset_deck(self):
-        self._cards = list(DECK)
-        random.shuffle(self._cards)
-        self._random_card = []
+    """Dealer can shuffle, deal cards, and determine players' postions
+    """
 
-    def deal_cards(self,player):
-        self._random_card = self._cards.pop()
-        player.add_card(self._random_card)
+    rank_list = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K',
+                 'A']
+
+    def __init__(self):
+        """Give dealer the deck
+
+        Notes:
+            deck: standard cards
+        """
+        super().__init__()
+        self.deck = init_standard_deck()
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    @staticmethod
+    def texas_sort(card_A, card_B):
+        """Sort the cards from the smaller to the greater
+        """
+        key = []
+        for card in [card_A, card_B]:
+            if card.rank == '':
+                key.append(TexasDealer.rank_list.index(card.suit))
+            else:
+                key.append(TexasDealer.rank_list.index(card.rank))
+        if key[0] > key[1]:
+            return 1
+        if key[0] < key[1]:
+            return -1
+        return 0
+    
+    def deal_hands(self, players):
+        """Deal two hidden hand cards to player
+
+        Args:
+            players: a list of Player objects
+        """
+        hand_num = 2
+        for index, player in enumerate(players):
+            player.hand = self.deck[index*hand_num:(index+1)*hand_num]
+            player.remained_cards = copy.deepcopy(player.hand)
+
+    def deal_cards(self, players):
+        """Deal visible cards to player
+
+        Args:
+            players: a list of Player objects
+        """
+        hand_num = 1
+        for index, player in enumerate(players):
+            player.hand = player.hand.append(self.deck[index*hand_num:(index+1)*hand_num])
+            player.remained_cards = copy.deepcopy(player.hand)
--- rlcard/games/texas-holdem/round.py
+++ rlcard/games/texas-holdem/round.py
@@ -1,181 +1,40 @@
 # -*- coding: utf-8 -*-
 """Implement Texasholdem Round class"""
-import sys
-from core import Round
-from dealer import TexasDealer
+import functools
+from rlcard.core import Round
+from rlcard.games.texasholdem.dealer import TexasDealer as Dealer
+from rlcard.games.texasholdem.judger import TexasJudger as Judger
 
 
 
 class TexasRound(Round):
-   def start_new_round(self):
-        self._round_count += 1
-        action = ''
-        player_is_in = True
-        bet_amount = 0
-        round_winner = -1
-        # round_winner not yet determined : -1
-        # Draw : 0 , house wins : 1 , player wins : 2
 
-        print ("---------------------ROUND", self._round_count , "-----------------------")
-        print (self._player.name , "has ", self._player.get_chips(), "chips")
-        print (self._house.name , "has ", self._house.get_chips(), "chips")
-        print ("Big blind is", self._big)
+    def __init__(self, players):
+        """
+        initialize the game, 
 
+        Args:
+            players: a list of Player objects
 
-        print ("Dealer has dealt 2 cards for each player")
-        self._deck.reset_deck()
-        self._deck.deal_to(self._player)
-        self._deck.deal_to(self._house)
-        self._deck.deal_to(self._player)
-        self._deck.deal_to(self._house)
-
-        # Put up an Ante before dealing cards
-        self.transfer_chips_and_display_cards(self._big)
-
-        player_is_in, action, bet_amount = self.ask_for_user_action()
-        if player_is_in:
-            print ("----------------------------------------------------")
-            print ("Player", action)
-            print ("This is the FLOP")
-            self._deck.deal_to(self._board)
-            self._deck.deal_to(self._board)
-            self._deck.deal_to(self._board)
-
-            self.transfer_chips_and_display_cards(bet_amount)
-            player_is_in, action, bet_amount = self.ask_for_user_action()
-        else:
-            pass
-
-        if player_is_in:
-            print ("----------------------------------------------------")
-            print ("Player", action)
-            print ("This is the TURN")
-            self._deck.deal_to(self._board)
-
-            self.transfer_chips_and_display_cards(bet_amount)
-            player_is_in, action, bet_amount = self.ask_for_user_action()
-        else:
-            pass
-
-        if player_is_in:
-            print ("----------------------------------------------------")
-            print ("Player", action)
-            print ("This is the RIVER")
-            self._deck.deal_to(self._board)
-
-            self.transfer_chips_and_display_cards(bet_amount)
-            player_is_in, action, bet_amount = self.ask_for_user_action()
-        else:
-            pass
-
-        if player_is_in:
-            print ("----------------------------------------------------")
-            print ("Player", action)
-            print ("This is the FINAL")
-            #TODO : Compare Hands and transfer money to winner
-            self.transfer_chips_and_display_cards(bet_amount)
-
-            #add 5 cards from board to each player
-            board_cards = self._board.get_cards()
-            player_cards = self._player.get_cards()
-            house_cards = self._house.get_cards()
-
-            self._player.add_card_to_hand(board_cards)
-            self._player.add_card_to_hand(player_cards)
-            self._house.add_card_to_hand(board_cards)
-            self._house.add_card_to_hand(house_cards)
+        Notes:
+            greater_player: the winner in one round
+            dealer: a instance of DouzizhuDealer
+            seen_cards: cards given to landlord after determining landlord
+            landlord_num: the id of landlord
+        """
+        self.greater_player = None
+        self.dealer = Dealer()
+  
+        seen_cards = self.dealer.deck[-3:]
+        seen_cards.sort(key=functools.cmp_to_key(Dealer.texas_sort))
+        # self.seen_cards = Judger.cards2str(seen_cards)
+        # print('seen cards:', self.seen_cards)
+       
 
 
-            #compare the hands
-            round_winner = self.compare_hands()
-
-            # transferring money to the winner
-            print ("\n")
-            if round_winner == 1:
-                print ("The house WINS this round")
-                self.transfer_chips(self._board , self._house, "all")
-            elif round_winner == 2:
-                print ("Player WINS this round")
-                self.transfer_chips(self._board , self._player, "all")
-            elif round_winner == 0:
-                print ("DRAWS , player and the house split the pot")
-                half = self._board.get_chips() / 2
-                self.transfer_chips(self._board , self._house, half)
-                self.transfer_chips(self._board , self._player, half)
-
-            house_5_cards = self._house.get_hand_five_cards()
-            player_5_cards = self._player.get_hand_five_cards()
-
-            print ("Common cards on table:" , board_cards)
-            print ("House has 2 cards:", house_cards)
-            print ("Player has 2 cards:" , player_cards)
-            print ("\n")
-            print ("House has", self._house.get_hand_name() , house_5_cards)
-            print ("Player has", self._player.get_hand_name() ,  player_5_cards)
-            print (self._player.name , ":", self._player.get_chips(), "chips." \
-                , self._house.name , ":", self._house.get_chips(), "chips. " \
-                , "Pot:", self._board.get_chips(), "chips")
-
-        else:
-            print ("Player folded. The house WINS this round")
-            self.transfer_chips(self._board , self._house, "all")
-            print (self._player.name , ":", self._player.get_chips(), "chips." \
-                , self._house.name , ":", self._house.get_chips(), "chips. " \
-                , "Pot:", self._board.get_chips(), "chips")
-
-        # reset players cards
-        self._player.reset_player_cards()
-        self._house.reset_player_cards()
-        self._board.reset_player_cards()
-
-
-    def compare_hands(self):
-        #evaluate player's hand
-        self._player.evaluate()
-        self._house.evaluate()
-
-        #compare hands
-        house_category = self._house.get_hand_category()
-        player_category = self._player.get_hand_category()
-        if house_category > player_category:
-            return 1
-        elif house_category < player_category:
-            return 2
-        elif house_category == player_category:
-            #compare equal category
-            house_5_cards = self._house.get_hand_five_cards()
-            player_5_cards = self._player.get_hand_five_cards()
-            for i in reversed(xrange(5)):
-                house_card_rank = house_5_cards[i][0]
-                player_card_rank = player_5_cards[i][0]
-                if RANKS.index(house_card_rank) > RANKS.index(player_card_rank):
-                    return 1
-                elif RANKS.index(house_card_rank) < RANKS.index(player_card_rank):
-                    return 2
-                else:
-                    pass
-            # this else below belongs to for..else.. statement
-            # return 0 means both hands are equal
-            else:
-                return 0
-
-
-    def avaliable_orders(self):
-        action = ''
-        action_list = ['C','R','F']
-
-        if self._player.get_chips() <= 0 or self._house.get_chips() <= 0 :
-            action_list = ['C','F']
-            print ("No more chips. Raise is not available")
-
-
-
-    def transfer_chips_and_display_cards(self, bet_amount):
-        self.transfer_chips(self._player , self._board, bet_amount)
-        self.transfer_chips(self._house , self._board, bet_amount)
-        self._player.display_cards()
-        self._house.display_cards()
-        self._board.display_cards()
-        print (self._player.name , ":", self._player.get_chips(), "chips." \
-            , self._house.name , ":", self._house.get_chips(), "chips. " \
-            , "Pot:", self._board.get_chips(), "chips")
+    def proceed_round(self, player, action):
+        """
+        Call other Classes's functions to keep one round running
+        """
+        self.greater_player = player.play(action, self.greater_player)
+        return self.greater_player
