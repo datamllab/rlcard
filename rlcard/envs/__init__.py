91109e78d039521a2f0fe71c13925164f594af35
Kwei-Herng Lai <khlai037@datalab3.engr.tamu.edu>
2019-08-30 09:38:35 -0700

    blackjack game

--- rlcard/envs/__init__.py
+++ rlcard/envs/__init__.py
@@ -1,8 +1,13 @@
 from rlcard.envs.regiteration import register, make
 
 ### Example... This one does work for now 
+register(
+    id='blackjack',
+    entry_point='rlcard.envs.blackjack:BlackjackEnv',
+)
 
 register(
     id='doudizhu',
     entry_point='rlcard.envs.doudizhu:DoudizhuEnv',
 )
+
