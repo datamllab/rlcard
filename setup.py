d40420745c7a4969409d11ed0062d86bc604d4a0
Daochen <daochen@datalab4.engr.tamu.edu>
2019-09-05 13:54:22 -0500

    Modify setup

--- setup.py
+++ setup.py
@@ -8,13 +8,12 @@ setuptools.setup(
     version="0.0.1",
     author="Data Analytics at Texas A&M (DATA) Lab",
     author_email="khlai037@tamu.edu",
-    description="A platform of Reinforcement learning on card games",
-    #url="https://github.com/pypa/sampleproject",
-    keywords=["Reinforcement", "Game", "RL", "AI"],
+    description="A Toolkit for Reinforcement Learning in Card Games",
+    url="https://github.com/datamllab/rlcard",
+    keywords=["Reinforcement Learning", "game", "RL", "AI"],
     packages=setuptools.find_packages(),
     install_requires=[
-        'numpy',
-        'tensorflow-gpu'
+        'tensorflow',
     ],
     classifiers=[
         "Programming Language :: Python :: 3.6",
