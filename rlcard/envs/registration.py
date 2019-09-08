90147376a1dd6be716d563191015b2c883919dbf
Daochen <daochen@datalab4.engr.tamu.edu>
2019-09-08 12:48:34 -0500

    docs

--- rlcard/envs/registration.py
+++ rlcard/envs/registration.py
@@ -2,88 +2,86 @@ import importlib
 
 
 class EnvSpec(object):
-	"""A specification for a particular instance of the environment.
-	"""
+    """A specification for a particular instance of the environment.
+    """
 
-	def __init__(self, id, entry_point=None):
-		""" Initilize
+    def __init__(self, id, entry_point=None):
+        """ Initilize
 
-		Args:
-			id (string): the name of the environent
-			entry_point (string): a string the indicates the location of the envronment class
-		"""
+        Args:
+            id (string): the name of the environent
+            entry_point (string): a string the indicates the location of the envronment class
+        """
 
 
-		self.id = id
-		mod_name, class_name = entry_point.split(':')
-		self._entry_point = getattr(importlib.import_module(mod_name), class_name)
+        self.id = id
+        mod_name, class_name = entry_point.split(':')
+        self._entry_point = getattr(importlib.import_module(mod_name), class_name)
 
-	def make(self):
-		"""Instantiates an instance of the environment
+    def make(self):
+        """Instantiates an instance of the environment
 
-		Returns:
-			env (Env): an instance of the environemnt
-		"""
+        Returns:
+            env (Env): an instance of the environemnt
+        """
 
-		env = self._entry_point()
-		return env
+        env = self._entry_point()
+        return env
 
 
 class EnvRegistry(object):
-	""" Register an environment (game) by ID
-	"""
+    """ Register an environment (game) by ID
+    """
 
-	def __init__(self):
-		""" Initilize
-		"""
+    def __init__(self):
+        """ Initilize
+        """
 
-		self.env_specs = {}
+        self.env_specs = {}
 
-	def register(self, id, entry_point):
-		""" Register an environment
+    def register(self, id, entry_point):
+        """ Register an environment
 
-		Args:
-			id (string): the name of the environent
-			entry_point (string): a string the indicates the location of the envronment class
-		"""
+        Args:
+            id (string): the name of the environent
+            entry_point (string): a string the indicates the location of the envronment class
+        """
 
-		if id in self.env_specs:
-			print ('Cannot re-register id: {}'.format(id))
-		self.env_specs[id] = EnvSpec(id, entry_point)
+        if id in self.env_specs:
+            print ('Cannot re-register id: {}'.format(id))
+        self.env_specs[id] = EnvSpec(id, entry_point)
 
-	def make(self, id):
-		""" Create and environment instance
+    def make(self, id):
+        """ Create and environment instance
 
-		Args:
-			id (string): the name of the environment
-		"""
+        Args:
+            id (string): the name of the environment
+        """
 
-		if id not in self.env_specs:
-			print ('Cannot find id: {}'.format(id))
-		return self.env_specs[id].make()
+        if id not in self.env_specs:
+            print ('Cannot find id: {}'.format(id))
+        return self.env_specs[id].make()
 
 # Have a global registry
 registry = EnvRegistry()
 
 
 def register(id, entry_point):
-	""" Register an environment
+    """ Register an environment
 
-	Args:
-		id (string): the name of the environent
-		entry_point (string): a string the indicates the location of the envronment class
-	"""
+    Args:
+        id (string): the name of the environent
+        entry_point (string): a string the indicates the location of the envronment class
+    """
 
     return registry.register(id, entry_point)
 
 def make(id):
-	""" Create and environment instance
+    """ Create and environment instance
 
-	Args:
-		id (string): the name of the environment
-	"""
-	
-	return registry.make(id)
-
-
+    Args:
+        id (string): the name of the environment
+    """
+    
+    return registry.make(id)
 
