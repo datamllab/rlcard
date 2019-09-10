e1e6674edbf368d9f8ad48124d759d66b32bc416
Daochen <daochen@datalab4.engr.tamu.edu>
2019-09-10 12:28:08 -0500

    dqn doudizhu
    Former-commit-id: b2d5ecfb3bcbdf18af9eccdb25816bb0bfb01900

--- examples/doudizhu_dqn.py
+++ examples/doudizhu_dqn.py
@@ -1,23 +1,25 @@
-""" A toy example of learning a Deep-Q Agent on Blackjack
+""" A toy example of learning a Deep-Q Agent on Dou Dizhu
 """
 
 import tensorflow as tf
 
 import rlcard
 from rlcard.agents.dqn_agent import DQNAgent
+from rlcard.agents.random_agent import RandomAgent
 from rlcard.utils.utils import *
 
 # Make environment
 env = rlcard.make('doudizhu')
+eval_env = rlcard.make('doudizhu')
 
 # Set the iterations numbers and how frequently we evaluate
-evaluate_every = 100
-evaluate_num = 1000
+evaluate_every = 10
+evaluate_num = 30
 episode_num = 1000000
 
 # Set the the number of steps for collecting normalization statistics
 # and intial memory size
-memory_init_size = 100
+memory_init_size = 1000
 norm_step = 100
 
 # Set a global seed
@@ -27,10 +29,15 @@ with tf.Session() as sess:
     # Set agents
     agent = DQNAgent(sess,
                        action_size=env.action_num,
+                       replay_memory_size=20000
                        replay_memory_init_size=memory_init_size,
                        norm_step=norm_step,
-                       state_shape=[6, 60])
+                       state_shape=[6, 60],
+                       mlp_layers=[1000, 1000])
     env.set_agents([agent, agent, agent])
+
+    random_agent = RandomAgent(action_size=eval_env.action_num)
+    eval_env.set_agents([agent, random_agent, random_agent])
 
     # Count the number of steps
     step_counter = 0
@@ -49,11 +56,11 @@ with tf.Session() as sess:
         if step_counter > memory_init_size + norm_step:
             agent.train()
 
-        # Evaluate the performance
+        # Evaluate the performance. Play with random agents.
         if episode % evaluate_every == 0:
             reward = 0
             for eval_episode in range(evaluate_num):
-                _, payoffs = env.run(is_training=False)
+                _, payoffs = eval_env.run(is_training=False)
                 reward += payoffs[0]
 
             print('\n########## Evaluation ##########')
