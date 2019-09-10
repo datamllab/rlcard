5b589e6bbdaf32d39dcf15d1224dffcd3488888e
Daochen <daochen@datalab4.engr.tamu.edu>
2019-09-10 15:26:06 -0500

    dqn test
    Former-commit-id: 127399e2a3db7fd80e91fdbd9609d9713690b0ae

--- examples/doudizhu_dqn.py
+++ examples/doudizhu_dqn.py
@@ -13,8 +13,8 @@ env = rlcard.make('doudizhu')
 eval_env = rlcard.make('doudizhu')
 
 # Set the iterations numbers and how frequently we evaluate
-evaluate_every = 100
-evaluate_num = 30
+evaluate_every = 1000
+evaluate_num = 200
 episode_num = 1000000
 
 # Set the the number of steps for collecting normalization statistics
@@ -32,11 +32,12 @@ with tf.Session() as sess:
                        replay_memory_size=20000,
                        replay_memory_init_size=memory_init_size,
                        norm_step=norm_step,
-                       state_shape=[6, 60],
-                       mlp_layers=[256, 256])
-    env.set_agents([agent, agent, agent])
+                       state_shape=[6, 5, 15],
+                       mlp_layers=[512, 512])
 
     random_agent = RandomAgent(action_size=eval_env.action_num)
+
+    env.set_agents([agent, random_agent, random_agent])
     eval_env.set_agents([agent, random_agent, random_agent])
 
     # Count the number of steps
@@ -52,9 +53,9 @@ with tf.Session() as sess:
             agent.feed(ts)
             step_counter += 1
 
-        # Train the agent
-        if step_counter > memory_init_size + norm_step:
-            agent.train()
+            # Train the agent
+            if step_counter > memory_init_size + norm_step:
+                agent.train()
 
         # Evaluate the performance. Play with random agents.
         if episode % evaluate_every == 0:
