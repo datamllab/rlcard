49423c9b55900e433806f50461f709e8576c6d61
Kwei-Herng Lai <khlai037@datalab3.engr.tamu.edu>
2019-09-09 17:50:06 -0700

    Add comments

--- rlcard/envs/blackjack.py
+++ rlcard/envs/blackjack.py
@@ -12,14 +12,29 @@ class BlackjackEnv(Env):
     """
 
     def __init__(self):
+        ''' Initialize the Blackjack environment
+        '''
         super().__init__(Game())
         self.rank2score = {"A":10, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}
         self.actions = ['hit', 'stand']
 
     def get_actions(self):
+        ''' Get all of available actions
+
+        Returns:
+            encoded_action_list (list): return encoded action list (from str to int)
+        '''
         return self.encode_action(self.actions)
 
     def extract_state(self, state):
+        ''' Extract the state representation from state dictionary for agent
+        
+        Args:
+            state (dict): Original state from the game
+
+        Returns:
+            observation (list): combine the player's score and dealer's observable score for observation
+        '''
         cards = state['state']
         my_cards = cards[0]
         dealer_cards = cards[1]
@@ -41,16 +56,25 @@ class BlackjackEnv(Env):
         return obs
 
     def encode_action(self, actions):
+        ''' Encode the action into action ids
+            
+        Args:
+            actions (list[str]): list of actions
+
+        Returns:
+            encoded_actions (list[int]): list of action ids
+        '''
         a = []
         for i, act in enumerate(actions):
             a.append(i)
         return a
 
-    def get_child_state(self, action):
-        next_state, next_player = self.step(action)
-        return next_state
-
     def get_payoffs(self):
+        '''Get the payoff of a game 
+
+        Returns:
+           payoffs (list): list of payoffs 
+        '''
         if self.game.winner['player'] == 0 and self.game.winner['dealer'] == 1:
             return [-1]
         elif self.game.winner['dealer'] == 0 and self.game.winner['player'] == 1:
@@ -61,5 +85,13 @@ class BlackjackEnv(Env):
             raise "There are some bugs!"
  
     def decode_action(self, action_id):
+        '''Decode the action for applying to the game
+
+        Args:
+            action id (int): action id
+
+        Returns:
+            action (str): action for the game 
+        '''
         return self.actions[action_id]
  
