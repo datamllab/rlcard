127399e2a3db7fd80e91fdbd9609d9713690b0ae
Daochen <daochen@datalab4.engr.tamu.edu>
2019-09-10 15:26:06 -0500

    dqn test

--- test/agents/test_dqn.py
+++ test/agents/test_dqn.py
@@ -6,8 +6,10 @@ from rlcard.agents.dqn_agent import *
 class TestUtilsMethos(unittest.TestCase):
 
     def test_init(self):
+        
+        sess = tf.InteractiveSession()
 
-        agent = DQNAgent(sess=tf.Session(),
+        agent = DQNAgent(sess=sess,
                          replay_memory_size=0,
                          replay_memory_init_size=0,
                          update_target_estimator_every=0,
@@ -27,4 +29,42 @@ class TestUtilsMethos(unittest.TestCase):
         self.assertEqual(agent.batch_size, 0)
         self.assertEqual(agent.action_size, 0)
         self.assertEqual(agent.norm_step, 0)
+
+        sess.close()
+        tf.reset_default_graph()
+
+    def test_train(self):
+        
+        norm_step = 100
+        memory_init_size = 100
+        step_num = 300
+
+        sess = tf.InteractiveSession()
+        agent = DQNAgent(sess=sess,
+                         action_size=2,
+                         replay_memory_init_size=memory_init_size,
+                         update_target_estimator_every=10,
+                         norm_step=norm_step,
+                         state_shape=[2])
+
+        for step in range(step_num):
+            ts = [np.random.random_sample((2,)), np.random.randint(2), 0, np.random.random_sample((2,)), True]
+            agent.feed(ts)
+            if step > norm_step + memory_init_size:
+                agent.train()
+
+        predicted_action = agent.eval_step(np.random.random_sample((2,))) 
+        self.assertGreaterEqual(predicted_action, 0)
+        self.assertLessEqual(predicted_action, 1)
+
+        step_action = agent.step(np.random.random_sample((2,))) 
+        self.assertGreaterEqual(predicted_action, 0)
+        self.assertLessEqual(predicted_action, 1)
+
+        sess.close()
+        tf.reset_default_graph()
+
+
+
+
 
