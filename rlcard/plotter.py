6d4cbd4e21d230a34c38782677c1266cab30be10
songyih <songyih1996@gmail.com>
2019-09-10 19:16:18 -0700

    init Plotter

---
+++ rlcard/plotter.py
@@ -0,0 +1,26 @@
+""" Game-related and Env-related base classes
+"""
+
+class Plotter(object):
+    """
+    Plotter saves the running results and helps make plots from the results
+    """
+
+    def __init__(self, xlabel = '', ylabel = '', legend = ''):
+        """
+
+        Args:
+
+        """
+        self.suit = suit
+        self.rank = rank
+
+    def add_point(self):
+        """
+
+        """
+
+    def make_plot(self, save_dir = ''):
+        """
+
+        """
