0288f4b1f41407f594b902e3c8acf9bc4ea3128b
Daochen <daochen@datalab4.engr.tamu.edu>
2019-09-11 08:36:08 -0500

    modify example
    Former-commit-id: 5a063d387dbaf15b51d387d0760569e868228358

--- examples/doudizhu_random.py
+++ examples/doudizhu_random.py
@@ -1,27 +1,24 @@
 """ A toy example of playing Doudizhu with random agents
 """
-# Example of using doudizhu environment
+
 import rlcard
 from rlcard.utils.utils import set_global_seed
 from rlcard.agents.random_agent import RandomAgent
 
 # Make environment
 env = rlcard.make('doudizhu')
-episode_num = 10
+episode_num = 2
 
-# Set global seed to 0
-set_global_seed(0)
+# Set a global seed
+set_global_seed(1)
 
 # Set up agents
-action_num = env.action_num
-agent_0 = RandomAgent(action_num)
-agent_1 = RandomAgent(action_num)
-agent_2 = RandomAgent(action_num)
-env.set_agents([agent_0, agent_1, agent_2])
+agent = RandomAgent(action_num=env.action_num)
+env.set_agents([agent, agent, agent])
 
 for episode in range(episode_num):
 
     # Generate data from the environment
-    trajectories, player_wins = env.run(False)
+    trajectories, player_wins = env.run(is_training=False)
     print(trajectories)
     print(player_wins)
