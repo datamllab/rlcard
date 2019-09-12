ca7e8fc88dc7cca38cd73e2c0cea65f75a472663
yuanpu_cao <yuanpucao@gmail.com>
2019-09-12 13:45:59 +0800

    add test of doudizhu

---
+++ test/games/test_doudizhu.py
@@ -0,0 +1,57 @@
+import unittest
+from rlcard.utils.utils import get_downstream_player_id, get_upstream_player_id
+from rlcard.games.doudizhu.game import DoudizhuGame as Game
+
+
+class TestDoudizhuMethods(unittest.TestCase):
+
+    def test_get_player_num(self):
+        game = Game()
+        player_num = game.get_player_num()
+        self.assertEqual(player_num, 3)
+
+    def test_get_action_num(self):
+        game = Game()
+        action_num = game.get_action_num()
+        self.assertEqual(action_num, 309)
+
+    def test_init_game(self):
+        game = Game()
+        state, current_player = game.init_game()
+        self.assertEqual(state['self'], current_player)
+        self.assertEqual(state['landlord'], current_player)
+        self.assertIs(state['played_cards'], None)
+
+    def test_step(self):
+        game = Game()
+        state, _ = game.init_game()
+        action = state['actions'][0]
+        state, next_player_id = game.step(action)
+        next_player = game.players[next_player_id]
+        player_id = get_upstream_player_id(next_player, game.players)
+        self.assertEqual(state['trace'][0][0], player_id)
+        self.assertEqual(state['trace'][0][1], action)
+
+    def test_get_player_id(self):
+        game = Game()
+        _, player_id = game.init_game()
+        current_player_id = game.get_player_id()
+        self.assertEqual(current_player_id, player_id)
+
+    def test_proceed_game(self):
+        game = Game()
+        state, player_id = game.init_game()
+        while not game.is_over():
+            action = state['actions'][0]
+            state, next_player_id = game.step(action)
+            player = game.players[player_id]
+            self.assertEqual(get_downstream_player_id(
+                player, game.players), next_player_id)
+            player_id = next_player_id
+        for player_id in range(3):
+            state = game.get_state(player_id)
+            self.assertIsNone(state['actions'])
+
+
+if __name__ == '__main__':
+    unittest.main()
