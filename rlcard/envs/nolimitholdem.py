40ccb4f26b8756059648cdccbd052bb59398146d
Songyi Huang <songyih1996@gmail.com>
2019-09-23 12:46:11 -0700

    nolimit holdem env
    Former-commit-id: b641964a1a3f9124fdf427668c93f430f88ab930

--- rlcard/envs/nolimitholdem.py
+++ rlcard/envs/nolimitholdem.py
@@ -5,6 +5,7 @@ import numpy as np
 import rlcard
 from rlcard.envs.env import Env
 from rlcard.games.nolimitholdem.game import NolimitholdemGame as Game
+from rlcard.envs.limitholdem import LimitholdemEnv
 from rlcard.utils.utils import *
 
 class NolimitholdemEnv(Env):
