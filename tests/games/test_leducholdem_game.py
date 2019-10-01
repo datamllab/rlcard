dad6d18ccf0142b18a2f1485bcb85271ae757597
Daochen <daochen@datalab4.engr.tamu.edu>
2019-10-01 12:16:29 -0500

    leduc test
    Former-commit-id: d92ef74ce0e27542c88747a886db4a0f30895968

--- tests/games/test_leducholdem_game.py
+++ tests/games/test_leducholdem_game.py
@@ -1,6 +1,9 @@
 import unittest
 
 from rlcard.games.leducholdem.game import LeducholdemGame as Game
+from rlcard.games.leducholdem.player import LeducholdemPlayer as Player
+from rlcard.games.leducholdem.judger import LeducholdemJudger as Judger
+from rlcard.core import Card
 
 class TestLeducholdemMethods(unittest.TestCase):
 
@@ -54,6 +57,51 @@ class TestLeducholdemMethods(unittest.TestCase):
         game.step_back()
         self.assertEqual(game.game_pointer, player_id)
         self.assertEqual(game.step_back(), False)
+    
+    def test_judge_game(self):
+        players = [Player(0), Player(1)]
+        players[0].in_chips = 10
+        players[1].in_chips = 10
+
+        # Test hand is equal
+        players[0].hand = Card('S', 'J')
+        players[1].hand = Card('H', 'J')
+        public_card = Card('S', 'Q')
+        payoffs = Judger.judge_game(players, public_card)
+        self.assertEqual(payoffs[0], 0)
+        self.assertEqual(payoffs[1], 0)
+
+        # Test one player get a pair
+        players[0].hand = Card('S', 'J')
+        players[1].hand = Card('S', 'Q')
+        public_card = Card('H', 'J')
+        payoffs = Judger.judge_game(players, public_card)
+        self.assertEqual(payoffs[0], 10.0)
+        self.assertEqual(payoffs[1], -10.0)
+
+        # Other cases
+        # Test one player get a pair
+        players[0].hand = Card('S', 'J')
+        players[1].hand = Card('S', 'Q')
+        public_card = Card('H', 'K')
+        payoffs = Judger.judge_game(players, public_card)
+        self.assertEqual(payoffs[0], -10.0)
+        self.assertEqual(payoffs[1], 10.0)
+
+    def test_player_get_player_id(self):
+        player = Player(0)
+        self.assertEqual(0, player.get_player_id())
+
+    def test_is_over(self):
+        game = Game()
+        game.init_game()
+        game.step('check')
+        game.step('check')
+        game.step('check')
+        game.step('check')
+        self.assertEqual(game.is_over(), True)
+
+
 
 
 if __name__ == '__main__':
