1a9ce778f56049a03e25eefaa0254f717523d3c8
Yuanpu <yuanpucao@gmail.com>
2019-10-05 10:16:08 +0800

    multi process
    Former-commit-id: e8c6127e28daaf16f3d5ecc2e5677325917fbf07

--- examples/doudizhu_random_multi_process.py
+++ examples/doudizhu_random_multi_process.py
@@ -1,13 +1,9 @@
-''' A toy example of playing Doudizhu with random agents in multi processes pool
-'''
-
 import time
 import multiprocessing
-import numpy as np
 
 import rlcard
 from rlcard.agents.random_agent import RandomAgent
-from rlcard.utils.utils import set_global_seed
+from rlcard.utils.utils import set_global_seed, assign_task
 
 if __name__ == '__main__':
     # Timer start
@@ -19,34 +15,38 @@ if __name__ == '__main__':
     # Set the number of process
     process_num = 8
 
-    # Initialize process pool
-    pool = multiprocessing.Pool(process_num)
-
-    # Set game and make environment
-    env = rlcard.make('doudizhu')
-
     # Set episode_num
     episode_num = 10000
+
+    # Assign tasks
+    per_tasks = assign_task(episode_num, process_num)
+
+    # Set game and make environment
+    game = 'doudizhu'
+    env = rlcard.make(game)
 
     # Set global seed
     set_global_seed(1)
 
     # Set up agents
-    agent_num = env.game.num_players
+    agent_num = env.player_num
     env.set_agents([RandomAgent(action_num=env.action_num)
                     for _ in range(agent_num)])
 
-    # Run game
-    trajectories_set = []
-    for episode in range(episode_num):
+    # Set a global list to reserve trajectories
+    manager = multiprocessing.Manager()
+    trajectories_set = manager.list()
 
-        # Generate data from the environment
-        result = pool.apply_async(env.run, args=(False, np.random.randint(10000000)))
-        trajectories_set.append(result)
-    for result in trajectories_set:
-        trajectories, player_wins = result.get()
-        # print(trajectories, player_wins)
+    # Generate Processes
+    processes = []
+    for p in range(process_num):
+        process = multiprocessing.Process(target=env.run_multi, args=(per_tasks[p], trajectories_set))
+        processes.append(process)
+
+    # Run process
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
     end = time.time()
-    pool.close()
-    pool.join()
     print('run time:', end-start)
