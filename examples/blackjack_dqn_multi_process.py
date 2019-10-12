9b410e79f2cd7ed206f0c23822efe58dd4109d3a
Yuanpu <yuanpucao@gmail.com>
2019-10-12 08:12:47 +0800

    support windows multiprocess

--- examples/blackjack_dqn_multi_process.py
+++ examples/blackjack_dqn_multi_process.py
@@ -2,6 +2,7 @@
 '''
 import numpy as np
 import tensorflow as tf
+import multiprocessing
 from multiprocessing import Process, JoinableQueue, Queue
 
 import rlcard
@@ -85,91 +86,94 @@ class BlackjackProcess(Process):
         self.sess.close()
         return
 
+if __name__ == '__main__':
+	# Avoid RuntimeError
+	multiprocessing.freeze_support()
 
-# Set a global seed
-set_global_seed(0)
+	# Set a global seed
+	set_global_seed(0)
 
-# Initialize processes
-PROCESS_NUM = 16
-INPUT_QUEUE = JoinableQueue()
-OUTPUT_QUEUE = Queue()
-PROCESSES = [BlackjackProcess(index, INPUT_QUEUE, OUTPUT_QUEUE, np.random.randint(1000000))
-             for index in range(PROCESS_NUM)]
-for p in PROCESSES:
-    p.start()
+	# Initialize processes
+	PROCESS_NUM = 16
+	INPUT_QUEUE = JoinableQueue()
+	OUTPUT_QUEUE = Queue()
+	PROCESSES = [BlackjackProcess(index, INPUT_QUEUE, OUTPUT_QUEUE, np.random.randint(1000000))
+				 for index in range(PROCESS_NUM)]
+	for p in PROCESSES:
+		p.start()
 
-# Make environment
-env = rlcard.make('blackjack')
-eval_env = rlcard.make('blackjack')
+	# Make environment
+	env = rlcard.make('blackjack')
+	eval_env = rlcard.make('blackjack')
 
-with tf.Session() as sess:
+	with tf.Session() as sess:
 
-    # Set agents
-    global_step = tf.Variable(0, name='global_step', trainable=False)
-    agent = DQNAgent(sess,
-                     scope='dqn',
-                     action_num=env.action_num,
-                     replay_memory_init_size=memory_init_size,
-                     norm_step=norm_step,
-                     state_shape=env.state_shape,
-                     mlp_layers=[10, 10])
-    env.set_agents([agent])
-    eval_env.set_agents([agent])
-    sess.run(tf.global_variables_initializer())
+		# Set agents
+		global_step = tf.Variable(0, name='global_step', trainable=False)
+		agent = DQNAgent(sess,
+						 scope='dqn',
+						 action_num=env.action_num,
+						 replay_memory_init_size=memory_init_size,
+						 norm_step=norm_step,
+						 state_shape=env.state_shape,
+						 mlp_layers=[10, 10])
+		env.set_agents([agent])
+		eval_env.set_agents([agent])
+		sess.run(tf.global_variables_initializer())
 
-    # Count the number of steps
-    step_counter = 0
+		# Count the number of steps
+		step_counter = 0
 
-    # Init a Logger to plot the learning curve
-    logger = Logger(xlabel='timestep', ylabel='reward',
-                    legend='DQN on Blackjack', log_path=log_path, csv_path=csv_path)
+		# Init a Logger to plot the learning curve
+		logger = Logger(xlabel='timestep', ylabel='reward',
+						legend='DQN on Blackjack', log_path=log_path, csv_path=csv_path)
 
-    for episode in range(episode_num // evaluate_every):
+		for episode in range(episode_num // evaluate_every):
 
-        # Generate data from the environment
-        tasks = assign_task(evaluate_every, PROCESS_NUM)
-        for task in tasks:
-            INPUT_QUEUE.put((task, True, None, None))
-        for _ in range(evaluate_every):
-            trajectories = OUTPUT_QUEUE.get()
+			# Generate data from the environment
+			tasks = assign_task(evaluate_every, PROCESS_NUM)
+			for task in tasks:
+				INPUT_QUEUE.put((task, True, None, None))
+			for _ in range(evaluate_every):
+				trajectories = OUTPUT_QUEUE.get()
 
-            # Feed transitions into agent memory, and train
-            for ts in trajectories[0]:
-                agent.feed(ts)
-                step_counter += 1
+				# Feed transitions into agent memory, and train
+				for ts in trajectories[0]:
+					agent.feed(ts)
+					step_counter += 1
 
-                # Train the agent
-                if step_counter > memory_init_size + norm_step:
-                    loss = agent.train()
-                    print('\rINFO - Step {}, loss: {}'.format(step_counter, loss), end='')
-        # Evaluate the performance
-        reward = 0
-        tasks = assign_task(evaluate_num, PROCESS_NUM)
-        variables = tf.contrib.slim.get_variables(scope="dqn", collection=tf.GraphKeys.TRAINABLE_VARIABLES)
-        variables = [var.eval() for var in variables]
-        for task in tasks:
-            INPUT_QUEUE.put((task, False, variables, agent.total_t))
-        for _ in range(evaluate_num):
-            payoffs = OUTPUT_QUEUE.get()
-            reward += payoffs[0]
-        logger.log('\n########## Evaluation ##########')
-        logger.log('Average reward is {}'.format(float(reward)/evaluate_num))
+					# Train the agent
+					if step_counter > memory_init_size + norm_step:
+						loss = agent.train()
+						print('\rINFO - Step {}, loss: {}'.format(step_counter, loss), end='')
+			# Evaluate the performance
+			reward = 0
+			tasks = assign_task(evaluate_num, PROCESS_NUM)
+			variables = tf.contrib.slim.get_variables(scope="dqn", collection=tf.GraphKeys.TRAINABLE_VARIABLES)
+			variables = [var.eval() for var in variables]
+			for task in tasks:
+				INPUT_QUEUE.put((task, False, variables, agent.total_t))
+			for _ in range(evaluate_num):
+				payoffs = OUTPUT_QUEUE.get()
+				reward += payoffs[0]
+			logger.log('\n########## Evaluation ##########')
+			logger.log('Average reward is {}'.format(float(reward)/evaluate_num))
 
-        # Add point to logger
-        logger.add_point(x=env.timestep, y=float(reward)/evaluate_num)
+			# Add point to logger
+			logger.add_point(x=env.timestep, y=float(reward)/evaluate_num)
 
-        # Make plot
-        if (episode*evaluate_every) % save_plot_every == 0 and episode > 0:
-            logger.make_plot(save_path=figure_path+str(episode)+'.png')
+			# Make plot
+			if (episode*evaluate_every) % save_plot_every == 0 and episode > 0:
+				logger.make_plot(save_path=figure_path+str(episode)+'.png')
 
-    # Make the final plot
-    logger.make_plot(save_path=figure_path+'final_'+str(episode)+'.png')
+		# Make the final plot
+		logger.make_plot(save_path=figure_path+'final_'+str(episode)+'.png')
 
-    # Close multi-processes
-    for _ in range(PROCESS_NUM):
-        INPUT_QUEUE.put(None)
+		# Close multi-processes
+		for _ in range(PROCESS_NUM):
+			INPUT_QUEUE.put(None)
 
-    INPUT_QUEUE.join()
+		INPUT_QUEUE.join()
 
-    for p in PROCESSES:
-        p.join()
+		for p in PROCESSES:
+			p.join()
