9d6c5bb46331fc835392d58c6446ba368748c6a9
Alexn0910 <54640216+Alexn0910@users.noreply.github.com>
2019-11-03 23:22:32 -0600

    Add files via upload

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,79 +1,28 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-header>Header</el-header>
-      <el-container>
-        <el-aside width="200px">
-          <el-row class="tac">
-            <el-col>
-              <el-menu
-                default-active="1"
-                class="el-menu-vertical-demo"
-                @open="handleOpen"
-                @close="handleClose">
-                <el-menu-item index="1">
-                  <i class="el-icon-menu"></i>
-                  <span>Game 1</span>
-                </el-menu-item>
-                <el-menu-item index="2">
-                  <i class="el-icon-menu"></i>
-                  <span>Game 2</span>
-                </el-menu-item>
-                <el-menu-item index="3">
-                  <i class="el-icon-menu"></i>
-                  <span>Game 3</span>
-                </el-menu-item>
-                <el-menu-item index="3">
-                  <i class="el-icon-menu"></i>
-                  <span>Game 4</span>
-                </el-menu-item>
-                <el-menu-item index="3">
-                  <i class="el-icon-menu"></i>
-                  <span>Game 5</span>
-                </el-menu-item>
-              </el-menu>
-            </el-col>
-          </el-row>
-        </el-aside>
-        <el-main>Main</el-main>
-      </el-container>
-    </el-container>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
 <style>
-  @import url("//unpkg.com/element-ui@2.12.0/lib/theme-chalk/index.css");
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
-  
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 500px;
-  }
-  
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-  
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-  
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
