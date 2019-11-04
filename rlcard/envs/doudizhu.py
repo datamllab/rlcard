cd8f02773826b0df34edbbc472340fa5a17720a0
Yuanpu <yuanpucao@gmail.com>
2019-11-04 10:57:13 +0800

    add rule for choosing kicker

--- rlcard/envs/doudizhu.py
+++ rlcard/envs/doudizhu.py
@@ -3,7 +3,7 @@ import numpy as np
 from rlcard.utils.utils import *
 from rlcard.envs.env import Env
 from rlcard.games.doudizhu.game import DoudizhuGame as Game
-from rlcard.games.doudizhu.utils import SPECIFIC_MAP
+from rlcard.games.doudizhu.utils import SPECIFIC_MAP, CARD_RANK_STR
 from rlcard.games.doudizhu.utils import ACTION_LIST, ACTION_SPACE
 from rlcard.games.doudizhu.utils import encode_cards
 
@@ -61,21 +61,35 @@ class DoudizhuEnv(Env):
             action (string): the action that will be passed to the game engine.
         '''
         abstract_action = ACTION_LIST[action_id]
+        # without kicker
+        if '*' not in abstract_action:
+            return abstract_action
+        # with kicker
         legal_actions = self.game.state['actions']
         specific_actions = []
+        kickers = []
         for legal_action in legal_actions:
             for abstract in SPECIFIC_MAP[legal_action]:
+                main = abstract.strip('*')
                 if abstract == abstract_action:
                     specific_actions.append(legal_action)
-        if specific_actions:
-            action = np.random.choice(specific_actions)
-        else:
-            if "pass" in legal_actions:
-                action = "pass"
-            else:
-                action = np.random.choice(legal_actions)
-
-        return action
+                    kickers.append(legal_action.replace(main, '', 1))
+                    break
+        # choose kicker with minimum score
+        kicker_scores = []
+        for kicker in kickers:
+            score = 0
+            for legal_action in legal_actions:
+                if kicker in legal_action:
+                    score += 1
+            kicker_scores.append(score+CARD_RANK_STR.index(kicker[0]))
+        min_index = 0
+        min_score = kicker_scores[0]
+        for index, score in enumerate(kicker_scores):
+            if score < min_score:
+                min_score = score
+                min_index = index
+        return specific_actions[min_index]
 
     def get_legal_actions(self):
         ''' Get all legal actions for current state
@@ -92,3 +106,15 @@ class DoudizhuEnv(Env):
                     if action_id not in legal_action_id:
                         legal_action_id.append(action_id)
         return legal_action_id
+
+
+# Test for decode_action
+#if __name__ == '__main__':
+#    env = DoudizhuEnv()
+#    env.init_game()
+#    env.game.state['actions'] = ['444', '33344', '33355']
+#    print(env.decode_action(54))
+#    env.game.state['actions'] = ['444', '33344', '33355']
+#    print(env.decode_action(29))
+#    env.game.state['actions'] = ['5', '6', '66', '55', '555', '33366', '33355']
+#    print(env.decode_action(54))
