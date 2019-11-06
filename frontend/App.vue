4c8da415aca3d92631d12c194c5df146db885556
Alexn0910 <54640216+Alexn0910@users.noreply.github.com>
2019-11-05 19:24:04 -0600

    Add files via upload

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,70 +1,139 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-header>Leaderboard</el-header>
+    <el-container style="height: 750px;">
+      <el-header>
+        <h1>
+          <font face='Arial'>
+            RLCard Leaderboard
+          </font>
+        </h1>
+      </el-header>
       <el-container>
         <el-aside width="200px">
           <el-row class="tac">
             <el-col>
-              <p>Games</p>
+              <h2 align='center'>
+                <span>
+                    <font 
+                      face='Arial'
+                      size='5'>  
+                      Games
+                    </font>  
+                  </span>
+              </h2>
               <el-menu
                 default-active="1"
-                class="el-menu-vertical-demo"
+                class="el-menu-vertical"
                 @open="handleOpen"
-                @close="handleClose">
+                @close="handleClose"
+                background-color=#E9EEF3>
                 <el-menu-item index="1">
                   <i class="el-icon-menu"></i>
-                  <span>Blackjack</span>
+                  <span>
+                    <font 
+                      face='Arial'
+                      color=#333>  
+                      Blackjack
+                    </font>  
+                  </span>
                 </el-menu-item>
                 <el-menu-item index="2">
                   <i class="el-icon-menu"></i>
-                  <span>Doudizhu</span>
+                  <span>
+                    <font 
+                      face='Arial'
+                      color=#333>  
+                      Doudizhu
+                    </font>  
+                  </span>
                 </el-menu-item>
                 <el-menu-item index="3">
                   <i class="el-icon-menu"></i>
-                  <span>Leduc Hold'em</span>
+                  <span>
+                    <font 
+                      face='Arial'
+                      color=#333>
+                      Leduc Hold'em
+                    </font>  
+                  </span>
                 </el-menu-item>
                 <el-menu-item index="4">
                   <i class="el-icon-menu"></i>
-                  <span>Limit Hold'em</span>
+                  <span>
+                    <font 
+                      face='Arial'
+                      color=#333> 
+                      Limit Hold'em
+                    </font>  
+                  </span>
                 </el-menu-item>
                 <el-menu-item index="5">
                   <i class="el-icon-menu"></i>
-                  <span>Mahjong</span>
+                  <span>
+                    <font 
+                      face='Arial'
+                      color=#333>
+                      Mahjong
+                    </font>  
+                  </span>
                 </el-menu-item>
                 <el-menu-item index="6">
                   <i class="el-icon-menu"></i>
-                  <span>No-Limit Hold'em</span>
+                  <span>
+                    <font 
+                      face='Arial'
+                      color=#333> 
+                      No-Limit Hold'em
+                    </font>  
+                  </span>
                 </el-menu-item>
                 <el-menu-item index="7">
                   <i class="el-icon-menu"></i>
-                  <span>Uno</span>
+                  <span>
+                    <font 
+                      face='Arial'
+                      color=#333>
+                      Uno
+                    </font>  
+                  </span>
                 </el-menu-item>
               </el-menu>
             </el-col>
           </el-row>
         </el-aside>
         <el-main>
-          <template>
+          <div align='center'>
             <el-table
               :data="tableData"
               style="width: 50%"
-              stripe='true'>
+              stripe="true">
               <el-table-column
                 prop="rank"
-                label="Rank">
+                label="Rank"
+                align='center'>
               </el-table-column>
               <el-table-column
                 prop="agent"
-                label="Agent">
+                label="Agent"
+                align='center'>
               </el-table-column>
               <el-table-column
                 prop="score"
-                label="Score">
+                label="Score"
+                align='center'>
               </el-table-column>
             </el-table>
-          </template>
+          </div>
         </el-main>
+      </el-container>
+      <el-container>
+        <el-footer>
+          <h2>
+            <font face='Arial'>
+              DATA Lab
+            </font>
+          </h2>
+        </el-footer>
       </el-container>
     </el-container>
   </div>
@@ -74,61 +143,47 @@
     export default {
       data() {
         return {
-          tableData: [{
-            rank: '1',
-            agent: 'NFSP',
-            score: '10,000'
-          }, {
-            rank: '2',
-            agent: 'DQN',
-            score: '9,500'
-          }, {
-            rank: '3',
-            agent: 'Deep CFR',
-            score: '7,000'
-          }, {
-            rank: '4',
-            agent: 'CFR',
-            score: '5,600'
-          }]
+          tableData: []
         }
+      },
+      mounted() {
+        this.axios.get("http://www.mocky.io/v2/5dc10315330000f22d1a4dad") 
+                  .then(response=>{ 
+                    this.tableData = response.data;
+                  }) 
       }
     }
   </script>
 
 <style>
-  @import url("//unpkg.com/element-ui@2.12.0/lib/theme-chalk/index.css");
   .el-header, .el-footer {
     background-color: #B3C0D1;
     color: #333;
     text-align: center;
-    line-height: 60px;
   }
   
   .el-aside {
     background-color: #D3DCE6;
     color: #333;
-    text-align: center;
-    line-height: 20px;
+    text-align: left;
   }
   
   .el-main {
     background-color: #E9EEF3;
     color: #333;
     text-align: center;
-    line-height: 20px;
+  }
+
+  h1 {
+    font-size: 2em;
+    margin-top: 0.35em;
+    font-weight: 500;
   }
   
-  body > .el-container {
-    margin-bottom: 40px;
+  h2 {
+    font-size: 1.1em;
+    margin-top: 1.2em;
+    font-weight: 500;
   }
   
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-  
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
 </style>
