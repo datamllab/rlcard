f03a5719d74503bdbdf70d0e9ede6b0443c09f2c
Daochen <daochen.zha@tamu.edu>
2020-03-04 14:40:51 -0600

    Use CFR as demo
    Former-commit-id: be9dfa29e9d4efc625f442b611c9bc628f473d06

--- rlcard/models/leducholdem_rule_models.py
+++ rlcard/models/leducholdem_rule_models.py
@@ -4,7 +4,7 @@ import rlcard
 from rlcard.models.model import Model
 from rlcard.games.leducholdem.game import LeducholdemGame
 
-class LeducholdemRuleAgentV1(object):
+class LeducHoldemRuleAgentV1(object):
     ''' Leduc Hold 'em Rule agent version 1
     '''
     def __init__(self):
@@ -19,10 +19,6 @@ class LeducholdemRuleAgentV1(object):
             action (str): Predicted action
         '''
         legal_actions = state['raw_legal_actions']
-        state = state['raw_obs']
-        hand = state['hand']
-        public_card = state['public_card']
-        action = 'fold'
         # Aggressively play 'raise' and 'call'
         if 'raise' in legal_actions:
             return 'raise'
@@ -33,61 +29,12 @@ class LeducholdemRuleAgentV1(object):
         else:
             return 'fold'
 
-    def step2(self, state):
-        ''' Predict the action when given raw state. A simple rule-based AI.
-        Args:
-            state (dict): Raw state from the game
-
-        Returns:
-            action (str): Predicted action
-        '''
-        legal_actions = state['raw_legal_actions']
-        state = state['raw_obs']
-        hand = state['hand']
-        public_card = state['public_card']
-        action = 'fold'
-        '''
-        When having only 2 hand cards at the game start, choose fold to drop terrible cards:
-        Acceptable hand cards:
-        Pairs
-        AK, AQ, AJ, AT
-        A9s, A8s, ... A2s(s means flush)
-        KQ, KJ, QJ, JT
-        Fold all hand types except those mentioned above to save money
-        '''
-        #if len(public_cards) == 0:
-        if public_card == None:
-            if hand[0] == 'K':
-                action = 'raise'
-            elif hand[0] == 'Q':
-                action = 'check'
-            else:
-                action = 'fold'
-        if public_card != None:
-            if public_cards[1] == hand[1]:
-                action = 'raise'
-            else:
-                action = 'fold'
-
-        #return action
-        if action in legal_actions:
-            return action
-        else:
-            if action == 'raise':
-                return 'call'
-            if action == 'check':
-                return 'fold'
-            if action == 'call':
-                return 'raise'
-            else:
-                return action
-
     def eval_step(self, state):
         ''' Step for evaluation. The same to step
         '''
         return self.step(state), []
 
-class LeducholdemRuleAgentV2(object):
+class LeducHoldemRuleAgentV2(object):
     ''' Leduc Hold 'em Rule agent version 2
     '''
     def __init__(self):
@@ -141,7 +88,7 @@ class LeducholdemRuleAgentV2(object):
             else:
                 return action
 
-class LeducholdemRuleModelV1(Model):
+class LeducHoldemRuleModelV1(Model):
     ''' Leduc holdem Rule Model version 1
     '''
 
@@ -149,7 +96,7 @@ class LeducholdemRuleModelV1(Model):
         ''' Load pretrained model
         '''
         env = rlcard.make('leduc-holdem')
-        rule_agent = LeducholdemRuleAgentV1()
+        rule_agent = LeducHoldemRuleAgentV1()
         self.rule_agents = [rule_agent for _ in range(env.player_num)]
 
     @property
@@ -163,3 +110,27 @@ class LeducholdemRuleModelV1(Model):
               functioning well.
         '''
         return self.rule_agents
+
+class LeducHoldemRuleModelV2(Model):
+    ''' Leduc holdem Rule Model version 2
+    '''
+
+    def __init__(self):
+        ''' Load pretrained model
+        '''
+        env = rlcard.make('leduc-holdem')
+        rule_agent = LeducHoldemRuleAgentV2()
+        self.rule_agents = [rule_agent for _ in range(env.player_num)]
+
+    @property
+    def agents(self):
+        ''' Get a list of agents for each position in a the game
+
+        Returns:
+            agents (list): A list of agents
+
+        Note: Each agent should be just like RL agent with step and eval_step
+              functioning well.
+        '''
+        return self.rule_agents
+
--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -1,7 +1,8 @@
 import unittest
 
 from rlcard.models.model import Model
-from rlcard.models.pretrained_models import LeducHoldemNFSPModel
+from rlcard.models.pretrained_models import LeducHoldemNFSPModel, LeducHoldemCFRModel
+from rlcard.models.leducholdem_rule_models import LeducHoldemRuleModelV1, LeducHoldemRuleModelV2
 
 class TestModel(unittest.TestCase):
 
@@ -12,6 +13,30 @@ class TestModel(unittest.TestCase):
     def test_leduc_holdem_nfsp_model(self):
         model = LeducHoldemNFSPModel()
         self.assertIsInstance(model, LeducHoldemNFSPModel)
+        self.assertIsInstance(model.agents, list)
+        
+    def test_leduc_holdem_cfr_model(self):
+        model = LeducHoldemCFRModel()
+        self.assertIsInstance(model, LeducHoldemCFRModel)
+        self.assertIsInstance(model.agents, list)
+
+    def test_leduc_holdem_rule_model_v1(self):
+        model = LeducHoldemRuleModelV1()
+        self.assertIsInstance(model, LeducHoldemRuleModelV1)
+        agent = model.agents[0]
+        action = agent.step({'raw_legal_actions':['raise']})
+        self.assertEqual(action, 'raise')
+        action = agent.step({'raw_legal_actions':['call']})
+        self.assertEqual(action, 'call')
+        action = agent.step({'raw_legal_actions':['check']})
+        self.assertEqual(action, 'check')
+        action = agent.step({'raw_legal_actions':[]})
+        self.assertEqual(action, 'fold')
+
+    def test_leduc_holdem_rule_model_v2(self):
+        model = LeducHoldemRuleModelV2()
+        self.assertIsInstance(model, LeducHoldemRuleModelV2)
+        # TODO: Add detailed tests
 
 if __name__ == '__main__':
     unittest.main()
