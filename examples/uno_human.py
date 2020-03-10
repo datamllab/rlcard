df18cc87b104d96d6377a8a6501b8a66578c3c26
Daochen <daochen.zha@tamu.edu>
2020-03-10 14:21:21 -0500

    Fix human

--- examples/uno_human.py
+++ examples/uno_human.py
@@ -3,7 +3,7 @@
 
 import rlcard
 from rlcard import models
-from rlcard.agents.uno_human_agent import HumanAgent
+from rlcard.agents.uno_human_agent import HumanAgent, _print_action
 
 # Make environment and enable human mode
 # Set 'record_action' to True because we need it to print results
@@ -17,7 +17,22 @@ print(">> UNO rule model V1")
 while (True):
     print(">> Start a new game")
 
-    _, payoffs = env.run(is_training=False)
+    trajectories, payoffs = env.run(is_training=False)
+    # If the human does not take the final action, we need to
+    # print other players action
+    final_state = trajectories[0][-1][-2]
+    action_record = final_state['action_record']
+    state = final_state['raw_obs']
+    _action_list = []
+    for i in range(1, len(action_record)+1):
+        if action_record[-i][0] == state['current_player']:
+            break
+        _action_list.insert(0, action_record[-i])
+    for pair in _action_list:
+        print('>> Player', pair[0], 'chooses ', end='')
+        _print_action(pair[1])
+        print('')
+
     print('===============     Result     ===============')
     if payoffs[0] > 0:
         print('You win!')
