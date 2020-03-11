c6697890540f0fd3e16363176c105c3ff159801d
Daochen <daochen.zha@tamu.edu>
2020-03-10 20:25:14 -0500

    Fix install issues
    Former-commit-id: 61d2a8ab91ff6819a06bc871d2bec50c2050e00b

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ setuptools.setup(
 	]},
     install_requires=[
         'numpy>=1.16.3',
-        'matplotlib>=3.0'
+        'matplotlib>=3.0',
+        'termcolor',
     ],
     extras_require=extras,
     requires_python='>=3.5',
