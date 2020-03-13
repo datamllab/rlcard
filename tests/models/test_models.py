dc4214642528387883032f2f8ab5f0f8ce4e1bef
Yuanpu <yuanpucao@gmail.com>
2020-03-13 14:23:47 +0800

    test doudizhu_rule_model
    Former-commit-id: d02dc3b5e4421bd362b9f6730914dd5cb281ad18

--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -5,6 +5,7 @@ from rlcard.models.pretrained_models import LeducHoldemNFSPModel, LeducHoldemNFS
 from rlcard.models.leducholdem_rule_models import LeducHoldemRuleModelV1, LeducHoldemRuleModelV2
 
 from rlcard.models.limitholdem_rule_models import LimitholdemRuleModelV1
+from rlcard.models.doudizhu_rule_models import DouDizhuRuleModelV1
 class TestModel(unittest.TestCase):
 
     def test_model(self):
@@ -82,5 +83,21 @@ class TestModel(unittest.TestCase):
         self.assertEqual(action, 'call')
         action = agent.step({'raw_legal_actions':['raise', 'fold', 'check', 'call'], 'raw_obs':{'hand':['SK', 'HQ'], 'public_cards':['H2', 'C2', 'B4', 'B5']}})
         self.assertEqual(action, 'fold')
+
+    def test_doudizhu_rule_model_v1(self):
+        model = DouDizhuRuleModelV1()
+        self.assertIsInstance(model, DouDizhuRuleModelV1)
+        agent = model.agents[0]
+        action = agent.step({'raw_obs': {'current_hand': '345567999TTTTQKAA2BR', 'trace':[]}})
+        self.assertEqual(action, '34567')
+        action = agent.step({'raw_obs': {'current_hand': '5999TTTTQKAA2BR', 'trace':[(0, '34567'), (1, 'pass'), (2, 'pass')]}})
+        self.assertEqual(action, '5')
+        action = agent.step({'raw_obs': {'actions': ['pass', '44455', '44466', 'TTTT', 'BR'], 'trace':[(0, '33344')]}})
+        self.assertEqual(action, '44455')
+        action = agent.step({'raw_obs': {'actions': ['pass', '44455', '44466', 'TTTT', 'BR'], 'trace':[(0, '33344'), [1, 'pass']]}})
+        self.assertEqual(action, '44455')
+        action = agent.step({'raw_obs': {'actions': ['pass', 'TTTT', 'BR'], 'self': 2, 'landlord': 0, 'trace':[(0, '33344'), (1, '55566')]}})
+        self.assertEqual(action, 'pass')
+
 if __name__ == '__main__':
     unittest.main()
