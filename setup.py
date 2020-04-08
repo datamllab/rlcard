4b630aff295927deef696d7dc96ec6c07921610f
Songyi Huang <songyih1996@gmail.com>
2020-04-07 22:37:49 -0700

    update version num
    Former-commit-id: bbb7f7f19d5fd14b8f083f109e775ccb973d272a

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ extras = {
 
 setuptools.setup(
     name="rlcard",
-    version="0.1.13",
+    version="0.1.14",
     author="Data Analytics at Texas A&M (DATA) Lab",
     author_email="khlai037@tamu.edu",
     description="A Toolkit for Reinforcement Learning in Card Games",
