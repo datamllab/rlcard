377a26243809bd6ce18ce92219d73d018aa1bccb
Daochen Zha <daochen.zha@tamu.edu>
2021-08-25 13:26:05 -0500

    Update dqn_agent.py

--- rlcard/agents/dqn_agent.py
+++ rlcard/agents/dqn_agent.py
@@ -414,24 +414,3 @@ class Memory(object):
         '''
         samples = random.sample(self.memory, self.batch_size)
         return map(np.array, zip(*samples))
-
-def copy_model_parameters(sess, estimator1, estimator2):
-    ''' Copys the model parameters of one estimator to another.
-
-    Args:
-        sess (tf.Session): Tensorflow Session object
-        estimator1 (Estimator): Estimator to copy the paramters from
-        estimator2 (Estimator): Estimator to copy the parameters to
-    '''
-    e1_params = [t for t in tf.trainable_variables() if t.name.startswith(estimator1.scope)]
-    e1_params = sorted(e1_params, key=lambda v: v.name)
-    e2_params = [t for t in tf.trainable_variables() if t.name.startswith(estimator2.scope)]
-    e2_params = sorted(e2_params, key=lambda v: v.name)
-
-    update_ops = []
-    for e1_v, e2_v in zip(e1_params, e2_params):
-        op = e2_v.assign(e1_v)
-        update_ops.append(op)
-
-    sess.run(update_ops)
-
