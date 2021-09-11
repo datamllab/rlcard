b28b0be30d55d254e5bb8e618430f193073b0e12
zhigal <razvilgrad@gmail.com>
2021-09-11 18:04:02 +1200

    LimitHoldemJudger class refactoring

--- rlcard/games/limitholdem/__init__.py
+++ rlcard/games/limitholdem/__init__.py
@@ -1,5 +1,5 @@
 from rlcard.games.limitholdem.dealer import LimitHoldemDealer as Dealer
-from rlcard.games.limitholdem.judger import LimitholdemJudger as Judger
+from rlcard.games.limitholdem.judger import LimitHoldemJudger as Judger
 from rlcard.games.limitholdem.player import LimitHoldemPlayer as Player
 from rlcard.games.limitholdem.player import PlayerStatus
 from rlcard.games.limitholdem.round import LimitHoldemRound as Round
--- rlcard/games/limitholdem/judger.py
+++ rlcard/games/limitholdem/judger.py
@@ -1,16 +1,16 @@
 from rlcard.games.limitholdem.utils import compare_hands
 import numpy as np
 
-class LimitholdemJudger:
-    ''' The Judger class for Texas Hold'em
-    '''
+
+class LimitHoldemJudger:
+    """The Judger class for limit texas holdem"""
+
     def __init__(self, np_random):
-        ''' Initialize a judger class
-        '''
         self.np_random = np_random
 
     def judge_game(self, players, hands):
-        ''' Judge the winner of the game.
+        """
+        Judge the winner of the game.
 
         Args:
             players (list): The list of players who play the game
@@ -18,10 +18,9 @@ class LimitholdemJudger:
 
         Returns:
             (list): Each entry of the list corresponds to one entry of the
-        '''
+        """
         # Convert the hands into card indexes
-        hands = [[card.get_index() for card in hand] if hand is not None else None
-                 for hand in hands]
+        hands = [[card.get_index() for card in hand] if hand is not None else None for hand in hands]
 
         winners = compare_hands(hands)
 
@@ -36,25 +35,31 @@ class LimitholdemJudger:
 
     def split_pot_among_players(self, in_chips, winners):
         """
-        to split the next (side)pot among players (
-        this function is called in loop by distribute_pots_among_players until all chips are allocated
-        :param in_chips: list with number of chips bet not yet distributed for each player
-        :param winners: list with 1 if the player is among winners else 0
-        :return: list of how much chips each player get after this pot has been split and list of chips left to distribute
+        Splits the next (side) pot among players.
+        Function is called in loop by distribute_pots_among_players until all chips are allocated.
+
+        Args:
+            in_chips (list): List with number of chips bet not yet distributed for each player
+            winners (list): List with 1 if the player is among winners else 0
+
+        Returns:
+            (list): Of how much chips each player get after this pot has been split and list of chips left to distribute
         """
-        nb_winners_in_pot = sum((winners[i] and in_chips[i]>0) for i in range(len(in_chips)))
-        nb_players_in_pot = sum(in_chips[i]>0 for i in range(len(in_chips)))
+        nb_winners_in_pot = sum((winners[i] and in_chips[i] > 0) for i in range(len(in_chips)))
+        nb_players_in_pot = sum(in_chips[i] > 0 for i in range(len(in_chips)))
         if nb_winners_in_pot == 0 or nb_winners_in_pot == nb_players_in_pot:
             # no winner or all winners for this pot
             allocated = list(in_chips)  # we give back their chips to each players in this pot
-            in_chips_after = len(in_chips)*[0]  # no more chips to distribute
+            in_chips_after = len(in_chips) * [0]  # no more chips to distribute
         else:
             amount_in_pot_by_player = min(v for v in in_chips if v > 0)
-            how_much_one_win, remaining = divmod(amount_in_pot_by_player*nb_players_in_pot, nb_winners_in_pot)
-            # In the event of a split pot that cannot be divided equally for every winner,
-            # the winner who is sitting closest to the left of the dealer receives the remaining differential in chips
-            # cf https://www.betclic.fr/poker/house-rules--play-safely--betclic-poker-cpok_rules
-            # to simplify and as this case is very rare, we will give the remaining differential in chips to a random winner
+            how_much_one_win, remaining = divmod(amount_in_pot_by_player * nb_players_in_pot, nb_winners_in_pot)
+            '''
+            In the event of a split pot that cannot be divided equally for every winner, the winner who is sitting 
+            closest to the left of the dealer receives the remaining differential in chips cf 
+            https://www.betclic.fr/poker/house-rules--play-safely--betclic-poker-cpok_rules to simplify and as this 
+            case is very rare, we will give the remaining differential in chips to a random winner
+            '''
             allocated = len(in_chips) * [0]
             in_chips_after = list(in_chips)
             for i in range(len(in_chips)):  # iterate on all players
@@ -64,26 +69,31 @@ class LimitholdemJudger:
                     allocated[i] += how_much_one_win
                 in_chips_after[i] -= amount_in_pot_by_player
             if remaining > 0:
-                random_winning_player = self.np_random.choice([i for i in range(len(winners)) if winners[i] and in_chips[i]>0])
+                random_winning_player = self.np_random.choice(
+                    [i for i in range(len(winners)) if winners[i] and in_chips[i] > 0])
                 allocated[random_winning_player] += remaining
-        assert sum(in_chips[i]-in_chips_after[i] for i in range(len(in_chips))) == sum(allocated)
+        assert sum(in_chips[i] - in_chips_after[i] for i in range(len(in_chips))) == sum(allocated)
         return allocated, in_chips_after
 
     def split_pots_among_players(self, in_chips_initial, winners):
         """
-        to split main pot and side pots among players (to handle special case of all-in players)
-        :param in_chips_initial: list with number of chips bet for each player
-        :param winners: list with 1 if the player is among winners else 0
-        :return: list of how much chips each player get back after all pots have been split
+        Splits main pot and side pots among players (to handle special case of all-in players).
+
+        Args:
+            in_chips_initial (list): List with number of chips bet for each player
+            winners (list): List with 1 if the player is among winners else 0
+
+        Returns:
+            (list): List of how much chips each player get back after all pots have been split
         """
         in_chips = list(in_chips_initial)
         assert len(in_chips) == len(winners)
-        assert all(v==0 or v==1 for v in winners)
+        assert all(v == 0 or v == 1 for v in winners)
         assert sum(winners) >= 1  # there must be at least one winner
         allocated = np.zeros(len(in_chips), dtype=int)
         while any(v > 0 for v in in_chips):  # while there are still chips to allocate
             allocated_current_pot, in_chips = self.split_pot_among_players(in_chips, winners)
             allocated += allocated_current_pot  # element-wise addition
-        assert all(chips >= 0 for chips in allocated)   # check that all players got a non negative amount of chips
+        assert all(chips >= 0 for chips in allocated)  # check that all players got a non negative amount of chips
         assert sum(in_chips_initial) == sum(allocated)  # check that all chips bet have been allocated
         return list(allocated)
