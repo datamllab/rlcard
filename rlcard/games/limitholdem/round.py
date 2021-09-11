74133926fe38da3d3b0ee4ee3fb24487c7f949af
zhigal <razvilgrad@gmail.com>
2021-09-11 17:54:13 +1200

    LimitHoldemRound class refactoring

--- rlcard/games/limitholdem/round.py
+++ rlcard/games/limitholdem/round.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
-''' Implement Limit Texas Hold'em Round class
-'''
-class LimitholdemRound:
-    ''' Round can call other Classes' functions to keep the game running
-    '''
+"""Limit texas holdem round class implementation"""
+
+
+class LimitHoldemRound:
+    """Round can call other Classes' functions to keep the game running"""
 
     def __init__(self, raise_amount, allowed_raise_num, num_players, np_random):
-        ''' Initilize the round class
+        """
+        Initialize the round class
 
         Args:
             raise_amount (int): the raise amount for each raise
             allowed_raise_num (int): The number of allowed raise num
             num_players (int): The number of players
-        '''
+        """
         self.np_random = np_random
         self.game_pointer = None
         self.raise_amount = raise_amount
@@ -29,15 +30,18 @@ class LimitholdemRound:
 
         # Raised amount for each player
         self.raised = [0 for _ in range(self.num_players)]
+        self.player_folded = None
 
-    def start_new_round (self, game_pointer, raised=None):
-        ''' Start a new bidding round
+    def start_new_round(self, game_pointer, raised=None):
+        """
+        Start a new bidding round
 
         Args:
+            game_pointer (int): The game_pointer that indicates the next player
             raised (list): Initialize the chips for each player
 
         Note: For the first round of the game, we need to setup the big/small blind
-        '''
+        """
         self.game_pointer = game_pointer
         self.have_raised = 0
         self.not_raise_num = 0
@@ -47,7 +51,8 @@ class LimitholdemRound:
             self.raised = [0 for _ in range(self.num_players)]
 
     def proceed_round(self, players, action):
-        ''' Call other Classes's functions to keep one round running
+        """
+        Call other classes functions to keep one round running
 
         Args:
             players (list): The list of players that play the game
@@ -55,7 +60,7 @@ class LimitholdemRound:
 
         Returns:
             (int): The game_pointer that indicates the next player
-        '''
+        """
         if action not in self.get_legal_actions():
             raise Exception('{} is not legal action. Legal actions: {}'.format(action, self.get_legal_actions()))
 
@@ -83,16 +88,17 @@ class LimitholdemRound:
 
         # Skip the folded players
         while players[self.game_pointer].status == 'folded':
-             self.game_pointer = (self.game_pointer + 1) % self.num_players
+            self.game_pointer = (self.game_pointer + 1) % self.num_players
 
         return self.game_pointer
 
     def get_legal_actions(self):
-        ''' Obtain the legal actions for the curent player
+        """
+        Obtain the legal actions for the current player
 
         Returns:
            (list):  A list of legal actions
-        '''
+        """
         full_actions = ['call', 'raise', 'fold', 'check']
 
         # If the the number of raises already reaches the maximum number raises, we can not raise any more
@@ -110,28 +116,12 @@ class LimitholdemRound:
         return full_actions
 
     def is_over(self):
-        ''' Check whether the round is over
+        """
+        Check whether the round is over
 
         Returns:
             (boolean): True if the current round is over
-        '''
+        """
         if self.not_raise_num >= self.num_players:
             return True
         return False
-
-#import numpy as np
-#from rlcard.games.limitholdem.player import LimitholdemPlayer as Player
-#
-#if __name__ == '__main__':
-#    players = [Player(0), Player(1)]
-#    game_pointer = 0
-#    r = LimitholdemRound(game_pointer, 2, 4)
-#    r.start_new_round(game_pointer=game_pointer, raised=[1, 2])
-#    print(r.raised, r.have_raised, r.not_raise_num)
-#
-#    while not r.is_over():
-#        legal_actions = r.get_legal_actions()
-#        action = np.random.choice(legal_actions)
-#        print(game_pointer, action, legal_actions)
-#        game_pointer = r.proceed_round(players[game_pointer], action)
-#        print(r.raised, r.have_raised, r.not_raise_num)
