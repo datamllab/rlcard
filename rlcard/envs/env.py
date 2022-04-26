100644fee083476fbc1f3ff456b630ebad23181e
hoang chau <hoangchau3008@gmail.com>
2022-04-26 16:27:37 +0700

    Update env.py

--- rlcard/envs/env.py
+++ rlcard/envs/env.py
@@ -30,7 +30,7 @@ class Env(object):
         # Game specific configurations
         # Currently only support blackjack、limit-holdem、no-limit-holdem
         # TODO support game configurations for all the games
-        supported_envs = ['blackjack', 'leduc-holdem', 'limit-holdem', 'no-limit-holdem']
+        supported_envs = ['blackjack', 'leduc-holdem', 'limit-holdem', 'no-limit-holdem', 'dummy']
         if self.name in supported_envs:
             _game_config = self.default_game_config.copy()
             for key in config:
